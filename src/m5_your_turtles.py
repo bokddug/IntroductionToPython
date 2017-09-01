62e4ab8d6f16492fe2bf3ce70328ff64b7420dcc
hyunj <hyunj@rose-hulman.edu>
2017-08-31 20:26:56 -0400

    m3

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -30,12 +30,26 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 ########################################################################
 import rosegraphics as rg
 window = rg.TurtleWindow()
-window.delay(20)
+window.delay(50)
 
-JJ= rg.SimpleTurtle()
+JA= rg.SimpleTurtle('turtle')
 JJ= rg.SimpleTurtle('turtle')
-JJ.pen =rg.Pen('red', 40)
+
+JA.pen =rg.Pen('blue', 4)
+JA.speed =10
+size =200
+for k in range(5):
+    JA.draw_square(200)
+    JA.pen_up()
+    JA.left(30)
+    JA.forward(80)
+    JA.right(30)
+    JA.pen_down()
+    size= size -5
+
+JJ.pen =rg.Pen('red', 10)
 JJ.speed = 10
+JJ.draw_circle(50)
 JJ.forward(50)
 JJ.left(50)
 JJ.backward(100)
